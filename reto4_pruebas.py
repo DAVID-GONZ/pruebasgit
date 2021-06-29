a1732d1ad20b0a99f6739493bcf6af9e028d69aa
davidg <dago92@hotmail.com>
2021-06-28 23:08:36 -0500

    Pruebas iniciales

---
+++ reto4_pruebas.py
@@ -0,0 +1,11 @@
+from pruebas import pruebas_codigo_estudiante
+
+
+def actualizar_estado_pestana(operaciones_usuario, pagina_default):
+
+
+    return pila_atras, pagina_actual, pila_adelante
+
+
+
+pruebas_codigo_estudiante(actualizar_estado_pestana)
